2388c9d17f3880eecc14c0607d5b9b10f5edf676
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-29 12:50:40 -0700

    Failed the test forEach for on loop until the end of array and return it

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
-const input = 5
-numArray = [];
-for ( let i = 0; i <= input; i++) {
-  numArray.push(i)
-}
-
-function numToString(input) {
-  if (input === 3) {
-    return "Won't you be my neighbor?"
-  } else if (input === 2) {
-    return "Boop!"
-  } else if (input === 1) {
-    return "Beep!"
-  } else {
-    return input;
+function numArray(num) {
+  let numArr = []; 
+  for (let i = 0; i <= num; i++) {
+    numArr.push(i);
   }
+  return numArr;
 }
 
 // now should loop the array on each ele then return words if it's 3,2,1 otherwise return that number
 
-outcome = new Array;
 
 
+function beMyNeighbor(num) {
+  const array = numArray(num);
+  console.log(array)
+  let outcome = new Array;
+
+  array.forEach(function(element) {
+    if ( array.includes(3)) {
+      outcome.push("Won't you be my neighbor?");
+    } else if ( array.includes(2)) {
+      outcome.push("Boop!");
+    } else if ( array.includes(1)) {
+      outcome.push("Beep!");
+    } else {
+      outcome.push(element);
+    }
+  });
+  return outcome;
+}
+
+beMyNeighbor(5)
 
 
-
-
+// function numToString(input) {
+//   if (input === 3) {
+//     return "Won't you be my neighbor?"
+//   } else if (input === 2) {
+//     return "Boop!"
+//   } else if (input === 1) {
+//     return "Beep!"
+//   } else {
+//     return input;
+//   }
+// }
